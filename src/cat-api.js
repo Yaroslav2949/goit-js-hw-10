11991e3fb349b9a0eabf448c102e228a5848f2f4
Yaroslav Dolynskyi <dolynskiy13@gmail.com>
2023-07-31 17:01:26 +0300

    commit 1.1

---
+++ src/cat-api.js
@@ -0,0 +1,26 @@
+const url = 'https://api.thecatapi.com/v1';
+const api_key =
+  "live_YrantmFJdAcnPCSJSPc216UJmIO4RjlbPCx294qBLhymUZ3xks2WKYT03y5VCzjt";
+
+export function fetchBreeds() {
+  return fetch(`${url}/breeds?api_key=${api_key}`)
+  .then(resp => {
+   
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
+}
+
+export function fetchCatByBreed(breedId){
+    // https://api.thecatapi.com/v1/images/search
+    return fetch(`${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`)
+    .then(resp => {
+   
+        if (!resp.ok) {
+          throw new Error(resp.statusText);
+        }
+        return resp.json();
+      });
+}
---
+++ src/index.js
@@ -0,0 +1,51 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import './css/styles.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+
+// fetchBreeds().then(data=> console.log(data))
+
+const selector = document.querySelector('.breed-select');
+const divCatInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+
+loader.classList.replace('loader', 'is-hidden');
+error.classList.add('is-hidden');
+divCatInfo.classList.add('is-hidden');
+
+let arrBreedsId = [];
+fetchBreeds().then(data => {
+  data.forEach(element => {
+    arrBreedsId.push({ text: element.name, value: element.id });
+  });
+  new SlimSelect({
+    select: selector,
+    data: arrBreedsId,
+  });
+});
+// .catch(onFetchError);
+
+selector.addEventListener('change', onSelectBreed);
+
+function onSelectBreed(evt) {
+  const breedId = evt.currentTarget.value;
+
+//   fetchCatByBreed(breedId).then(data => console.log(data));
+
+fetchCatByBreed(breedId).then(data => (divCatInfo.innerHTML= createMarkup(data.breeds.url))) ;
+
+}
+
+function createMarkup(arr) {
+  return arr.map(
+    ({url, breeds:{name,description,temperament }}) =>
+      `<div class="box-img"><img src="${url}" alt="${breeds.name}" width="400"/></div>
+      <div class="box">
+      <h1>${breeds.name}</h1>
+      <p>${breeds.description}</p>
+      <p><b>Temperament:</b> ${breeds.temperament}</p>
+      </div>`
+  ).join('')
+}
