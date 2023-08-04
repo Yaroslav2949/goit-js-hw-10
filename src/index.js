cf179a67d9596321378cc57b29a7b6fc8ecf0f5b
Yaroslav Dolynskyi <dolynskiy13@gmail.com>
2023-08-04 15:44:57 +0300

    commit 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
-import './css/styles.css';
+import './styles.css';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 // fetchBreeds().then(data=> console.log(data))
@@ -16,45 +16,60 @@ error.classList.add('is-hidden');
 divCatInfo.classList.add('is-hidden');
 
 let arrBreedsId = [];
-fetchBreeds().then(data => {
-  data.forEach(element => {
-    arrBreedsId.push({ text: element.name, value: element.id });
-  });
-
-  new SlimSelect({
-    select: selector,
-    data: arrBreedsId,
-  });
-});
+fetchBreeds()
+  .then(data => {
+    data.forEach(element => {
+      arrBreedsId.push({ text: element.name, value: element.id });
+    });
+    new SlimSelect({
+      select: selector,
+      data: arrBreedsId,
+    });
+  })
+  .catch(onFetchError);
 
 selector.addEventListener('change', onSelectBreed);
 
 function onSelectBreed(event) {
+  loader.classList.replace('is-hidden', 'loader');
+  selector.classList.add('is-hidden');
+  divCatInfo.classList.add('is-hidden');
+
   const breedId = event.currentTarget.value;
   fetchCatByBreed(breedId)
-  .then(data =>console.log (data))
-  
-  .then(data => (divCatInfo.innerHTML=createMarkup(data[0]))
-  
-  )
-  .catch(onFetchError);
-};
+    .then(data => {
+      loader.classList.replace('loader', 'is-hidden');
+      selector.classList.remove('is-hidden');
 
-function createMarkup(arr) {
-  
-  return arr.map(
-    ({[0]:{url}, [0]:{breeds,[0]:{name,description,temperament }}}) =>
-      `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div>
-      <div class="box">
-      <h1>${breeds[0].name}</h1>
-      <p>${breeds[0].description}</p>
-      <p><b>Temperament:</b> ${breeds[0].temperament}</p>
-      </div>`
-  ).join('')
+      divCatInfo.innerHTML = createMarkup(data[0]);
+      divCatInfo.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
 }
-function onFetchError(error){
-  Notify.failure( `Oops! Something went wrong! Try reloading the page!`,{
-    timeout:5000,
-    position: 'center-center',
-  });
-};
+
+function createMarkup(data) {
+  const {
+    url,
+    breeds: [{ name, description, temperament }],
+  } = data;
+
+  return `<div class="box-img"><img src="${url}" alt="${name}" width="400"/></div>
+      <div class="box">
+      <h1>${name}</h1>
+      <p>${description}</p>
+      <p><b>Temperament:</b> ${temperament}</p>
+      </div>`;
+}
+
+function onFetchError(error) {
+  selector.classList.remove('is-hidden');
+  loader.classList.replace('loader', 'is-hidden');
+
+  Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!',
+    {
+      position: 'center-center',
+      timeout: 5000,
+    }
+  );
+}
